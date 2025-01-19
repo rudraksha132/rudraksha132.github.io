3493d908a1ddb188646ed56a87f8731ea62ce369
Rudraksha <141563051+rudraksha132@users.noreply.github.com>
2025-01-19 19:02:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.classList.add('dark-theme');
-} else {
-    document.documentElement.classList.add('light-theme');
-}
+
