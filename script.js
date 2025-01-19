cfc0e0b7d16844a5551d20eb5806997a8d5a6d25
Rudraksha <141563051+rudraksha132@users.noreply.github.com>
2025-01-19 23:57:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function getDeviceTheme() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return 'dark';
+  } else {
+    return 'light';
+  }
+}
 
+const theme = getDeviceTheme();
+alert(theme);
